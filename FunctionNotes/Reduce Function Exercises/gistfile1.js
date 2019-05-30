390675d93e236b948202be1c917692b102d10ca3
esajc <chihimjc@gmail.com>
2019-05-29 23:17:00 -0700

    Map - reduce practise

--- FunctionNotes/Reduce Function Exercises/gistfile1.js
+++ FunctionNotes/Reduce Function Exercises/gistfile1.js
@@ -6,17 +6,37 @@
 // Write a function capitalize that takes a string and uses .map to return the same string in all caps. 
 // ex. capitalize('whoop') // => 'WHOOP'
 // ex. capitalize('oh hey gurl') // => "OH HEY GURL"
-var capitalize = function(string){
-  // code code code!
+const capitalize = function(string){
+  const stringArr = Array.from(string);
+  const upperCaseString = stringArr.map(word => {
+    return word.toUpperCase();
+  })
+  return upperCaseString.join('');
 }
+
+// console.log(capitalize('This is a sentence that is going be be uppercased.'));
 
 // Now write a new function called swapCase that takes a string of words and uses .map and your newly written capitalize() 
 // function to return a string where every other word is in all caps. 
 // Hint: look up Array.prototype.map on MDN and see what arguments the .map callback can take. 
 // ex: swapCase('hey gurl, lets javascript together sometime') // => "HEY gurl, LETS javascript TOGETHER sometime"
-var swapCase = function(string){
-  // Codeeeee
+var swapCase = function(string = 'Default string') {
+  const stringArr = string.split(' ');
+  const newSentence = stringArr.map(function (word, index) {
+    // Can try using a reduce function in here as well instead of +=
+    let swapped = '';
+    if (index % 2) {
+      swapped += capitalize(word);
+    } else {
+      swapped += word;
+    }
+    return swapped;
+  });
+
+  return newSentence; 
 }
+
+console.log(swapCase('wow this sentence is really tripping me out!'));
 
 // Write a function shiftLetters that takes a string and uses .map to return an encoded string with each letter shifted down the 
 // alphabet by one. Hint: Use Look up the JS functions String.fromCharCode() and String.CharCodeAt() and see if you can use
